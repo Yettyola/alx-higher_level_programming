43170be550fc95083ea26381c63da0623c8cd282
yettyola <yettyolukemi@gmail.com>
2023-01-10 03:35:16 -0800

    task 1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" class MyList """
+
+
+class MyList(list):
+    """ class MyList that inherits from list """
+
+    def __init__(self):
+        """ initialize a Rectangle """
+        super().__init__()
+
+    def print_sorted(self):
+        """ prints the list, but sorted (ascending sort) """
+        sort_list = self.copy()
+        sort_l
+ist.sort()
+        print(sort_list)
