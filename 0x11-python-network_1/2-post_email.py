cb50721af52b752b4a3426b19d814061a24ee739
yettyola <yettyolukemi@gmail.com>
2023-04-16 12:35:05 -0700

    task 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
-"""displays the value of the X-Request-Id variable found in
-the header of the response.
-"""
+"""Send POST"""
+import urllib.request
+import urllib.parse
+import sys
 
+
+def sender():
+    """sender"""
+    v = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(v)
+    data = data.encode("ascii")
+    req = urllib.request.Request(sys.argv[1], data)
+    with urllib.request.urlopen(req) as response:
+        html = response.read()
+        print(html.decode("utf-8"))
 
 if __name__ == "__main__":
-    from urllib.request import urlopen, Request
-    from urllib.parse import urlencode
-    from sys import argv
-
-    value = {"email": argv[2]}
-    request = Request(
-            argv[1], urlencode(value).encode("ascii"))
-    with urlopen(request) as response:
-        head = respo
-nse.headers.get('X-Request-Id')
-        print(response.read().decode('utf-8'))
+    sender()
