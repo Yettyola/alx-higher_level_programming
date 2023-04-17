d4f409526acfb0d21b2264440c690f433f581e13
root <yettyolukemi@gmail.com>
2023-04-17 03:18:20 -0700

    task 10

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,12 +1,22 @@
 #!/usr/bin/python3
-"""displays the value of the X-Request-Id variable found in
-the header of the response.
-"""
+"""Check status"""
+import requests
+from requests.auth import HTTPBasicAuth
+import sys
 
+
+def searchapi():
+    """status"""
+    user = str(sys.argv[1])
+    pw = str(sys.argv[2])
+    result = requests.get("https://api.github.com/user",
+                          auth=(HTTPBasicAuth(user, pw)))
+
+    try:
+        data = result.json()
+        print(data["id"])
+    except:
+        print("None")
 
 if __name__ == "__main__":
-    from requests import get
-    from sys import argv
-
-    r = get('https://api.github.com/user', auth=(argv[1], argv[2]))
-    print(r.json().get('id'))
+    searchapi()
