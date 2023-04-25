f563ecb82d10c211c3dddb7a634d70106a19f216
yettyola <yettyolukemi@gmail.com>
2023-04-25 23:31:43 +0000

    task 6

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,19 +1,22 @@
 #!/usr/bin/node
-// JS Script
-require('request').get(process.argv[2], function (err, r, body) {
-  if (err) {
-    console.log(err);
-  } else {
-    let res = {};
-    let temp = JSON.parse(body);
-    for (let i = 0; i < temp.length; i++) {
-      if (temp[i].completed) {
-        if (!(temp[i].userId in res)) {
-          res[temp[i].userId] = 0;
-        }
-        res[temp[i].userId] += 1;
+
+const request = require('request');
+
+request(process.argv[2], function (error, response, body) {
+  if (error) {
+    console.error(error);
+  }
+  const dict = JSON.parse(body).reduce((acc, elem) => {
+    if (!acc[elem.userId]) {
+      if (elem.completed) {
+        acc[elem.userId] = 1;
+      }
+    } else {
+      if (elem.completed) {
+        acc[elem.userId] += 1;
       }
     }
-    console.log(res);
-  }
+    return acc;
+  }, {});
+  console.log(dict);
 });
