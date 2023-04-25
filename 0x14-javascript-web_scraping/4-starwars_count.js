217ff739c090587a4dfdcb199949df382e4e47c2
yettyola <yettyolukemi@gmail.com>
2023-04-25 23:28:20 +0000

    task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,19 +1,20 @@
 #!/usr/bin/node
-// JS Script
-require('request').get(process.argv[2], function (err, r, body) {
-  if (err) {
-    console.log(err);
-  } else {
-    let counter = 0;
-    let data = JSON.parse(body).results;
-    for (let i = 0; i < data.length; i++) {
-      for (let n = 0; n < data[i].characters.length; n++) {
-        if (data[i].characters[n].includes('/18/')) {
-          counter++;
-          break;
-        }
-      }
-    }
-    console.log(counter);
+
+const request = require('request');
+
+request(process.argv[2], function (error, response, body) {
+  if (error) {
+    console.error(error);
   }
+  /* const nb = JSON.parse(body).results.reduce((acc, elem) => {
+    acc += elem.characters.reduce((acc, character) => {
+      return (character === 'https://swapi.co/api/people/18/' ? acc + 1 : acc);
+    }, 0);
+    return (acc);
+  }, 0);
+  */
+  const nb = JSON.parse(body).results.filter((elem) => {
+    return elem.characters.filter((url) => { return url.includes('18'); }).length;
+  }).length;
+  console.log(nb);
 });
