e119e63cbcb3ffa1987abf158b10bda58b9c34d8
yettyola <yettyolukemi@gmail.com>
2023-05-04 14:05:43 +0000

    task 7

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -1,4 +1,7 @@
-// JQuery
-$.get('https://swapi.co/api/people/5/?format=json', function (data) {
-  $('div#character').text(data.name);
+$.ajax({
+  type: 'GET',
+  url: 'https://swapi-api.alx-tools.com/api/people/5/?format=json',
+  success: function (data) {
+    $('DIV#character').text(data.name);
+  }
 });
